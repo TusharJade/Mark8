14df4b1d802d953e15eb6d4bd9d284ee8e8e11a4
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-09-27 11:18:14 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+var emojiDictonary = {
+  "🙈": "See-No-Evil Monkey",
+  "🦁": "lion",
+  "🙉": "Hear-No-Evil Monkey",
+  "🙊": "Speak-No-Evil Monkey",
+  "🔥": "fire",
+  "🌈": "Rainbow",
+  "💧": "Droplet",
+  "🦀": "Crab"
+};
+
 export default function App() {
+  var [no, now] = useState("");
+
+  function clik(e) {
+    if (e.target.input === undefined) {
+      emojiDictonary[e.target.value] = "We dont have this in our databae";
+    }
+    now(emojiDictonary[e.target.value]);
+  }
+  console.log(no);
   return (
     <div className="App">
-      <h1>Welcome</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Inside outtt !</h1>
+      <input className="t" onChange={clik}></input>
+      <div>{no}</div>
     </div>
   );
 }
